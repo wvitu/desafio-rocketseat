9ab996c8c8d798a68ae5ad828367e18847346b62
Wanderson Vitor <wandersonvitor96@gmail.com>
2025-09-25 16:57:49 -0300

    Primeiro commit: estrutura inicial do projeto

---
+++ src/main/java/spring/boot/desafio/rocketseat/RocketseatApplication.java
@@ -0,0 +1,13 @@
+package spring.boot.desafio.rocketseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketseatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketseatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/boot/desafio/rocketseat/RocketseatApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.desafio.rocketseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketseatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
